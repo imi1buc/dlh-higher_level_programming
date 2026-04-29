abcaeb80af0e2b4f5210dcd7ce8ac56a39192903
Ionut Mircea <imircea78@gmail.com>
2026-04-29 08:03:51 +0000

    Bug fixing script 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(" ".join(map(str, row)))
+        formatted_row = " ".join("{:d}".format(num) for num in row)
+        print("{}".format(formatted_row))
