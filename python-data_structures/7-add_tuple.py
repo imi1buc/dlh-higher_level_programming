36939c9ae0fc3dd9813ce42ce0fb9fbaa961a359
Ionut Mircea <imircea78@gmail.com>
2026-04-29 16:21:20 +0000

    Adding script 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    string_length = min(max(len(tuple_a), len(tuple_b)), 2)
     new_list = []
-    for i in range(string_length):
+    for i in range(0, 2):
         val_a = tuple_a[i] if i < len(tuple_a) else 0
         val_b = tuple_b[i] if i < len(tuple_b) else 0
         new_list.append(val_a + val_b)
