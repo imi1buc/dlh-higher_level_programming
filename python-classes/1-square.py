e93de5993fe55387e7a07bd2a4b739b374c68501
imi1buc <imircea78@gmail.com>
2026-05-06 03:15:27 +0000

    Adding supporting script and script for 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This is a new module"""
+
+
+class Square:
+    """Purpose of this class is to compute the square"""
+    def __init__(self, size):
+        # Defining size attribute
+        self.__size = size
