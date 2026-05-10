62bfc8708a6249c73e3128da26596601d39760c8
imi1buc <imircea78@gmail.com>
2026-05-10 15:23:03 +0000

    Adding script 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Reading a file"""
+
+def read_file(filename=""):
+    # Reading the file
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
