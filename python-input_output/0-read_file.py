9525d182b202cf2351a834661b3d68003e1fc91d
imi1buc <imircea78@gmail.com>
2026-05-10 15:33:51 +0000

    Bug fixing and text additions

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
-    # Reading the file
+    """Reads contents of a text file and print to stdout."""
     with open(filename, encoding='utf-8') as file:
+        # This is the content
         print(file.read(), end='')
